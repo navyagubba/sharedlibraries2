550026a8e9e4e0204fcb8af76faab7afd4765329
navyagubba <48706834+navyagubba@users.noreply.github.com>
2023-05-01 16:10:17 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -16,4 +16,9 @@ sh  "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@$
 def executeSelenium(jobname)
 {
  sh  "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
-}       
+}  
+
+def newDeploy(jobname,ipaddress,context)
+{
+sh  "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
+}
