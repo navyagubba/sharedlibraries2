cac671ccdb4576553b4e44b993981e74a5bdec15
navyagubba <48706834+navyagubba@users.noreply.github.com>
2023-05-02 12:02:44 +0530

    Update and rename cicd2.groovy to cicd2.groovy

---
+++ vars/cicd2.groovy
@@ -0,0 +1,20 @@
+
+def newGit(repo)
+{
+ git /${repo}"
+}
+
+def newMaven()
+{
+sh 'mvn package'
+}
+
+def newDeploy(jobname,ipaddress,context)
+{
+sh  "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def executeSelenium(jobname)
+{
+ sh  "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
